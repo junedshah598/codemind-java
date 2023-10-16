8efb4c8d60bdc4d150ef5080dbc3031f9dbad182
MD Juned Shah <147886618+junedshah598@users.noreply.github.com>
2023-10-16 11:44:59 +0530

    import java.util.Scanner; public class Main{     public static void main(String args[]){         Scanner in=new Scanner(System.in);         float A=in.nextFloat();         float B=in.nextFloat();         System.out.printf("%.2f",A*B);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        float A=in.nextFloat();
+        float B=in.nextFloat();
+        System.out.printf("%.2f",A*B);
+    }
+}
